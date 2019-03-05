a6e19e1376ac9d97f4bfc654284174a4c54ada94
Khoi Nguyen Minh <intern.nmkhoi@tma.com.vn>
2019-03-05 17:29:15 +0700

    @mappedsuperclass

--- src/main/java/com/khoi/basecrud/dto/baseDTO.java
+++ src/main/java/com/khoi/basecrud/dto/baseDTO.java
@@ -7,12 +7,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
-@Entity
+@MappedSuperclass
 public class baseDTO implements Serializable {
+
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
   @Column(name = "id")
